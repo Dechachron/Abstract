06a9402037c7608cf97f038fd22589b99d672fe4
DCN <116252572+Dechachron@users.noreply.github.com>
2023-01-11 18:13:32 +0530

    Update abstract.sol

--- abstract.sol
+++ abstract.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.6.12;
 
 abstract contract Context {
     function _msgSender() internal view virtual returns (address) {
